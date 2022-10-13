55f9ef2b3142ad4f951cac94f33906286371e277
Edgar Sanchez-Barreda <edgarsanchez@Edgars-MBP.attlocal.net>
2022-10-12 22:26:17 -0700

    Creating new branch for different styling.

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import { Info } from './Info';
 
 export const Home = () => {
     return (
-        <div className='h-screen bg-gradient-to-tl from-white to-pink-100'>
+        <div className='h-full bg-gradient-to-tl from-white to-pink-100'>
             <Navbar />
             <Hero />
             <Info />
